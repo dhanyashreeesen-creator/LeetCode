487742801eceda819aa3d0d5975c8b8d65644d42
Dhanyashree Sen <dhanyashreeesen@gmail.com>
2026-08-20 05:22:35 +0530

    LC-subarray-sum-equals-k [Runtime: N/A; Memory: N/A]

--- array/subarray-sum-equals-k.c
+++ array/subarray-sum-equals-k.c
@@ -1,17 +1,65 @@
 int subarraySum(int* nums, int numsSize, int k)
 {
-    int count = 0;
+    int capacity = 1;
 
-    for(int i = 0; i < numsSize; i++)
+    while (capacity < 2 * numsSize + 1)
+        capacity *= 2;
+
+    long long keys[capacity];
+    int freq[capacity];
+
+    for (int i = 0; i < capacity; i++)
     {
-        int sum = 0;
+        keys[i] = 0;
+        freq[i] = 0;
+    }
 
-        for(int j = i; j < numsSize; j++)
+    int count = 0;
+    long long sum = 0;
+
+    // Prefix sum 0 has occurred once
+    int pos = 0;
+    keys[pos] = 0;
+    freq[pos] = 1;
+
+    for (int i = 0; i < numsSize; i++)
+    {
+        sum += nums[i];
+
+        long long needed = sum - k;
+
+        // Search for (sum - k)
+        unsigned long long h = (unsigned long long)needed;
+        int index = h & (capacity - 1);
+
+        while (freq[index] != 0)
         {
-            sum += nums[j];
+            if (keys[index] == needed)
+            {
+                count += freq[index];
+                break;
+            }
 
-            if(sum == k)
-                count++;
+            index = (index + 1) & (capacity - 1);
+        }
+
+        // Insert/update current prefix sum
+        h = (unsigned long long)sum;
+        index = h & (capacity - 1);
+
+        while (freq[index] != 0 && keys[index] != sum)
+        {
+            index = (index + 1) & (capacity - 1);
+        }
+
+        if (freq[index] == 0)
+        {
+            keys[index] = sum;
+            freq[index] = 1;
+        }
+        else
+        {
+            freq[index]++;
         }
     }
 
